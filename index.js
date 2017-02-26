b5b9b6ab47c9629d220c7f8ee4c3466bfb8192bd
zsevic <zeljkosevic95@gmail.com>
2017-02-26 21:05:00 +0100

    updated answering

--- index.js
+++ index.js
@@ -24,36 +24,48 @@ app.post('/webhook/',function(req,res){
     for (var i=0;i<messaging_events.length; i++) {
         var event=req.body.entry[0].messaging[i];
         var sender=event.sender.id;
-        setTimeout(function(){
+        /*setTimeout(function(){
                 console.log("typing...");
-        },5000);
-	if(event.message && event.message.attachments[0].type==="location"){
-			var lat=event.message.attachments[0].payload.coordinates.lat;
-			var long=event.message.attachments[0].payload.coordinates.long;
-			distance(lat,long,function(err,res){
-                sendLocation(sender, res);
-			});
-        }
-    }
+        },5000);*/
+		if(event.message && event.message.attachments){
+			if(event.message.attachments[0].type==="location"){
+				var lat=event.message.attachments[0].payload.coordinates.lat;
+				var long=event.message.attachments[0].payload.coordinates.long;
+				distance(lat,long,function(err,res){
+    	    		sendMessage(sender,res);
+				});
+    	    }else{
+				sendMessage(sender,"Posaljite svoju lokaciju");
+			}
+   		}else if(event.message.text){
+			var text=event.message.text;
+			sendMessage(sender,"Posaljite svoju lokaciju");
+		}
+	}
     res.sendStatus(200);
 });
 
-function sendLocation(sender, loc) {
-    var messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": {
-                    "element": {
-                        "title": "Najbliza Telenor prodavnica",
-                        "image_url": "https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center="+loc.lat+","+loc.long+"&zoom=17&markers="+loc.lat+","+loc.long,
-                        "item_url":"http:\/\/maps.apple.com\/maps?q="+loc.lat+","+loc.long+"&z=16"
-                    }
-                }
-            }
-        }
-    };
+function sendMessage(sender,msg) {
+    var messageData;
+	if(msg.lat && msg.long){
+		messageData={
+        	"attachment": {
+        	    "type": "template",
+        	    "payload": {
+        	        "template_type": "generic",
+        	        "elements": {
+        	            "element": {
+        	                "title": "Najbliza Telenor prodavnica",
+        	                "image_url": "https:\/\/maps.googleapis.com\/maps\/api\/staticmap?size=764x400&center="+msg.lat+","+msg.long+"&zoom=17&markers="+msg.lat+","+msg.long,
+        	                "item_url":"http:\/\/maps.apple.com\/maps?q="+msg.lat+","+msg.long+"&z=16"
+        	            }
+        	        }
+        	    }
+        	}
+    	};
+	}else{
+		messageData={text:msg};
+	}
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
